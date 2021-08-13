ab6ab0a51a0b761b30cc4098324dda732d86e68f
souravverma <souravverma@gmail.com>
2021-08-13 13:49:55 +0530

    Add files via upload

--- src/api/dataService.js
+++ src/api/dataService.js
@@ -1,1 +1,105 @@
 
+
+export default function() {
+  // simulates data coming from a database.
+  return Promise.resolve(
+    [
+        {
+          custid: 1,
+          name: "Sourav",
+          amt: 120,
+          transactionDt: "07-01-2021"
+        },
+        {
+          custid: 1,
+          name: "Sourav",
+          amt: 75,
+          transactionDt: "07-21-2021"
+        },
+        {
+          custid: 1,
+          name: "Sourav",
+          amt: 94,
+          transactionDt: "07-21-2021"
+        },
+        {
+          custid: 1,
+          name: "Sourav",
+          amt: 10,
+          transactionDt: "06-01-2021"
+        },
+        {
+          custid: 1,
+          name: "Sourav",
+          amt: 75,
+          transactionDt: "06-21-2021"
+        },
+        {
+          custid: 1,
+          name: "Sourav",
+          amt: 200,
+          transactionDt: "08-01-2021"
+        },
+        {
+          custid: 1,
+          name: "Sourav",
+          amt: 1,
+          transactionDt: "08-04-2021"
+        },
+        {
+          custid: 1,
+          name: "Sourav",
+          amt: 80,
+          transactionDt: "08-03-2021"
+        },
+        {
+          custid: 1,
+          name: "Sourav",
+          amt: 224,
+          transactionDt: "08-10-2021"
+        },
+        {
+          custid: 2,
+          name: "Gourav",
+          amt: 125,
+          transactionDt: "07-01-2021"
+        },
+        {
+          custid: 2,
+          name: "Gourav",
+          amt: 75,
+          transactionDt: "07-21-2021"
+        },
+        {
+          custid: 2,
+          name: "Gourav",
+          amt: 10,
+          transactionDt: "06-01-2021"
+        },
+        {
+          custid: 2,
+          name: "Gourav",
+          amt: 75,
+          transactionDt: "06-21-2021"
+        },
+        {
+          custid: 2,
+          name: "Gourav",
+          amt: 200,
+          transactionDt: "08-01-2021"
+        },
+        {
+          custid: 2,
+          name: "Gourav",
+          amt: 224,
+          transactionDt: "08-21-2021"
+        },
+        {
+          custid: 3,
+          name: "Sonia",
+          amt: 120,
+          transactionDt: "06-21-2021"
+        }
+    ]
+  );
+};
